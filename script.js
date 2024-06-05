e893052783ced3669ea11c758e22b0567c89381d
Enoch <49845680+enoch-xxiv@users.noreply.github.com>
2024-06-05 14:43:16 +0100

    add generateGrid function

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const container = document.querySelector("#container");
+const generateButton = document.querySelector("#newGrid");
 
-for (i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    div.classList.toggle("square");
-    container.appendChild(div);
-};
-
-const squares = document.querySelectorAll(".square");
-
-squares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "rgb(87, 103, 73)";
+function generateGrid(gridLength) {
+    for (i = 0; i < (gridLength ** 2); i++) {
+        const div = document.createElement("div");
+        div.classList.toggle("square");
+        container.appendChild(div);
+    };
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "rgb(87, 103, 73)";
+        });
     });
+}
+
+generateButton.addEventListener("click", () => {
+    let gridLength = prompt("How long would you like the square grid to be?");
+    generateGrid(gridLength);
 });
