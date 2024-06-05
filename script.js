6106f4ee755b90a8129152d6a8a922ec42b82111
Enoch <49845680+enoch-xxiv@users.noreply.github.com>
2024-06-05 12:07:39 +0100

    change color change function

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const container = document.querySelector("#container");
 
-for (i = 0; i <  256; i++) {
+for (i = 0; i < 256; i++) {
     const div = document.createElement("div");
-    div.classList.toggle("square")
+    div.classList.toggle("square");
     container.appendChild(div);
-}
+};
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "rgb(87, 103, 73)";
+    });
+});
